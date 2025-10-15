96dbae6c98342c6c7af82b992a622adef01a84aa
Ztyph000 <kevin.segura.ksou@gmail.com>
2025-10-14 19:11:28 -0600

    Delete Prog5-3C2025 directory

--- Prog5-3C2025/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Prog5_3C2025.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
